12e711088133a729413d5806617df547cf8bbcee
Kranthikumarmunjala <Kranthiq3@gmail.com>
2026-02-05 11:46:46 +0530

    Added new site data

--- solar_analyzer/__init__.py
+++ solar_analyzer/__init__.py
@@ -1,2 +1,9 @@
+# import pymysql
+# pymysql.install_as_MySQLdb()
+
+
 import pymysql
+
+# Trick Django into thinking we are using a newer version of mysqlclient
+pymysql.version_info = (2, 2, 1, "final", 0)
 pymysql.install_as_MySQLdb()
